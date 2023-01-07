ce9b6b2411a2f29809b515e0cb2d8aad5b964fca
Hovik Melikyan <hovik.melikyan@gmail.com>
2023-01-07 21:48:35 +0100

    Initial commit, minimal SwiftUI project

---
+++ AsyncMuxDemo/AsyncMuxDemo/AsyncMuxDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncMuxDemoApp.swift
+//  AsyncMuxDemo
+//
+//  Created by Hovik Melikyan on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncMuxDemoApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
