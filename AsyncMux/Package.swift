612740621f6dfad250caa7befdf329778d048a81
Hovik Melikyan <hovik.melikyan@gmail.com>
2023-01-20 17:39:34 +0100

    Fixed the package description; enabled Mac Catalyst but removed Mac support for now

--- AsyncMux/Package.swift
+++ AsyncMux/Package.swift
@@ -3,19 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "AsyncMux",
+	name: "AsyncMux",
 	platforms: [.iOS(.v15), .macOS(.v12)],
-    products: [
-        .library(
-            name: "AsyncMux",
-            targets: ["AsyncMux"]),
-    ],
-    dependencies: [
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "AsyncMux",
-            dependencies: []),
-    ]
+	products: [
+		.library(
+			name: "AsyncMux",
+			targets: ["AsyncMux"]),
+	],
+	dependencies: [
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: [
+		.target(
+			name: "AsyncMux",
+			dependencies: [],
+			path: "Sources"
+		),
+	]
 )
