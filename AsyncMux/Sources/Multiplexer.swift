fa560a7907a950fdaebfe4ed0d8db47698048786
Hovik Melikyan <hovik.melikyan@gmail.com>
2023-04-21 01:50:05 +0400

    Formatting: removed trailing spaces (where are they coming from?)

--- AsyncMux/Sources/Multiplexer.swift
+++ AsyncMux/Sources/Multiplexer.swift
@@ -22,30 +22,26 @@ private let muxRootDomain = "_Root.Domain"
 /// See README.md for a more detailed discussion.
 ///
 public actor Multiplexer<T: Codable & Sendable>: MuxRepositoryProtocol {
-    
+
     public typealias OnFetch = @Sendable () async throws -> T
-    
+
     public let cacheKey: String
-    
-    ///
+
     /// Instantiates a `Multiplexer<T>` object with a given `onFetch` block.
     /// - parameter cacheKey (optional): a string to be used as a file name for the disk cache. If omitted, an automatic name is generated based on `T`'s description. NOTE: if you have several  multiplexers whose `T` is the same, you *should* define unique non-conflicting `cacheKey` parameters for each.
     /// - parameter onFetch: an async throwing block that should retrieve an object presumably in an asynchronous manner.
-    ///
     public init(cacheKey: String? = nil, onFetch: @escaping OnFetch) {
         self.cacheKey = cacheKey ?? String(describing: T.self)
         self.onFetch = onFetch
     }
-    
-    ///
+
     /// Performs a request either by calling the `onFetch` block supplied in the multiplexer's constructor, or by returning the previously cached object, if available. Multiple simultaneous calls to `request()` are handled by the Multiplexer so that only one `onFetch` operation can be invoked at a time, but all callers of `request()` will eventually receive the result.
-    ///
     public func request() async throws -> T {
         return try await fetcher.request(domain: muxRootDomain, key: cacheKey) { [self] in
             try await onFetch()
         }
     }
-    
+
     /// "Soft" refresh: the next call to `request()` will attempt to retrieve the object again, without discarding the caches in case of a failure. `refresh()` does not have an immediate effect on any ongoing asynchronous requests. Can be chained with the subsequent `request()`.
     @discardableResult
     public func refresh(_ flag: Bool = true) -> Self {
@@ -54,7 +50,7 @@ public actor Multiplexer<T: Codable & Sendable>: MuxRepositoryProtocol {
         }
         return self
     }
-    
+
     /// Writes the previously cached object to disk.
     public func save() {
         if fetcher.isDirty, let storedValue = fetcher.storedValue {
@@ -62,17 +58,17 @@ public actor Multiplexer<T: Codable & Sendable>: MuxRepositoryProtocol {
             fetcher.isDirty = false
         }
     }
-    
+
     public func clearMemory() {
         fetcher.clearMemory()
     }
-    
+
     /// Clears the memory and disk caches. Will trigger a full fetch on the next `request()` call.
     public func clear() {
         MuxCacher.delete(domain: muxRootDomain, key: cacheKey)
         clearMemory()
     }
-    
+
     private let onFetch: OnFetch
     private var fetcher = _MuxFetcher<String, T>()
 }
@@ -86,31 +82,27 @@ public actor Multiplexer<T: Codable & Sendable>: MuxRepositoryProtocol {
 /// See README.md for a more detailed discussion.
 ///
 public actor MultiplexerMap<K: MuxKey, T: Codable & Sendable>: MuxRepositoryProtocol {
-    
+
     public typealias OnKeyFetch = @Sendable (K) async throws -> T
-    
+
     public let cacheKey: String
-    
-    ///
+
     /// Instantiates a `MultiplexerMap<T>` object with a given `onKeyFetch` block.
     /// - parameter cacheKey (optional): a string to be used as a file name for the disk cache. If omitted, an automatic name is generated based on `T`'s description. NOTE: if you have several  multiplexers whose `T` is the same, you *should* define unique non-conflicting `cacheKey` parameters for each.
     /// - parameter onKeyFetch: an async throwing block that should retrieve an object by a given key presumably in an asynchronous manner.
-    ///
     public init(cacheKey: String? = nil, onKeyFetch: @escaping OnKeyFetch) {
         self.cacheKey = cacheKey ?? String(describing: T.self)
         self.onKeyFetch = onKeyFetch
     }
-    
-    ///
+
     /// Performs a request either by calling the `onKeyFetch` block supplied in the multiplexer's constructor, or by returning the previously cached object, if available. Multiple simultaneous calls to `request(key:)` are handled by the MultiplexerMap so that only one `onKeyFetch` operation can be invoked at a time for any given `key`, but all callers of `request(key:)` will eventually receive the result.
-    ///
     public func request(key: K) async throws -> T {
         let fetcher = fetcherForKey(key)
         return try await fetcher.request(domain: cacheKey, key: key) { [self] in
             try await onKeyFetch(key)
         }
     }
-    
+
     /// "Soft" refresh: the next call to `request(key:)` will attempt to retrieve the object again, without discarding the caches in case of a failure. `refresh(key:)` does not have an immediate effect on any ongoing asynchronous requests. Can be chained with the subsequent `request(key:)`.
     @discardableResult
     public func refresh(_ flag: Bool = true, key: K) -> Self {
@@ -119,7 +111,7 @@ public actor MultiplexerMap<K: MuxKey, T: Codable & Sendable>: MuxRepositoryProt
         }
         return self
     }
-    
+
     /// "Soft" refresh for all objects stored in this `MultiplexerMap`: the next call to `request(key:)` with any key will attempt to retrieve the object again, without discarding the caches in case of a failure. `refresh()` does not have an immediate effect on any ongoing asynchronous requests. Can be chained with the subsequent `request(key:)`.
     @discardableResult
     public func refresh(_ flag: Bool = true) -> Self {
@@ -130,7 +122,7 @@ public actor MultiplexerMap<K: MuxKey, T: Codable & Sendable>: MuxRepositoryProt
         }
         return self
     }
-    
+
     /// Writes all previously cached objects in this `MultiplexerMap` to disk.
     public func save() {
         fetcherMap.forEach { key, fetcher in
@@ -140,30 +132,30 @@ public actor MultiplexerMap<K: MuxKey, T: Codable & Sendable>: MuxRepositoryProt
             }
         }
     }
-    
+
     public func clearMemory(key: K) {
         fetcherMap.removeValue(forKey: key)
     }
-    
+
     public func clearMemory() {
         fetcherMap = [:]
     }
-    
+
     /// Clears the memory and disk caches for an object with a given `key`. Will trigger a full fetch on the next `request(key:)` call.
     public func clear(key: K) {
         MuxCacher.delete(domain: cacheKey, key: String(key))
         clearMemory(key: key)
     }
-    
+
     /// Clears the memory and disk caches all objects in this `MultiplexerMap`. Will trigger a full fetch on the next `request(key:)` call.
     public func clear() {
         MuxCacher.deleteDomain(cacheKey)
         clearMemory()
     }
-    
+
     private let onKeyFetch: OnKeyFetch
     private var fetcherMap: [K: _MuxFetcher<K, T>] = [:]
-    
+
     private func fetcherForKey(_ key: K) -> _MuxFetcher<K, T> {
         var fetcher = fetcherMap[key]
         if fetcher == nil {
@@ -178,16 +170,16 @@ public actor MultiplexerMap<K: MuxKey, T: Codable & Sendable>: MuxRepositoryProt
 // MARK: - _MuxFetcher (internal)
 
 final private class _MuxFetcher<K: MuxKey, T: Codable & Sendable> {
-    
+
     typealias OnFetch = @Sendable () async throws -> T
-    
+
     var storedValue: T?
     var isDirty: Bool = false
     var refreshFlag: Bool = false
-    
+
     private var task: Task<T, Error>?
     private var completionTime: TimeInterval = 0
-    
+
     func request(domain: String, key: K, onFetch: @escaping OnFetch) async throws -> T {
         if !refreshFlag, !isExpired {
             if let storedValue {
@@ -198,9 +190,9 @@ final private class _MuxFetcher<K: MuxKey, T: Codable & Sendable> {
                 return cachedValue
             }
         }
-        
+
         refreshFlag = false
-        
+
         if task == nil {
             let storedValue = storedValue // silence the sendability warning
             task = Task {
@@ -215,7 +207,7 @@ final private class _MuxFetcher<K: MuxKey, T: Codable & Sendable> {
                 }
             }
         }
-        
+
         do {
             let result = try await task!.value
             task = nil
@@ -231,12 +223,12 @@ final private class _MuxFetcher<K: MuxKey, T: Codable & Sendable> {
             throw error
         }
     }
-    
+
     func clearMemory() {
         completionTime = 0
         storedValue = nil
     }
-    
+
     private var isExpired: Bool {
         Date().timeIntervalSinceReferenceDate > completionTime + defaultTTL
     }
